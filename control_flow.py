73453f0bccd3c29c2f5070e0ba2d6f54b984493c
tivereidoro <tivereidoro@yahoo.com>
2024-01-03 13:28:58 +0000

    Still remixing again

--- control_flow.py
+++ control_flow.py
@@ -1,1 +1,19 @@
-# EXercise: 
+# EXercise:
+# Create list (1,2,3,4,5)  and iterate thru, and if value is 2, print 'the value is 2' or 'the value is not 2' if it isn't.
+
+# If the value is 5, run a while looop to print 'last item' 6 times.
+
+
+num_list = [1, 2, 3, 4, 5]
+
+#Use for loop to iterate  thru;
+for number in num_list:
+    if number == 2:
+        print('The value is 2')
+    elif number == 5:
+        repeat = 0
+        while repeat < 7:
+            print('last item')
+            repeat++
+    else:
+        print('The value is not 2')
