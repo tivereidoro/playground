6df171cc72c197c36c1b2ea3eefc09b02470064d
tivereidoro <tivereidoro@yahoo.com>
2024-01-06 16:52:25 +0000

    ALX revision

--- trials.py
+++ trials.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
-
 # Test
 
 str = 'Holberton school'
-
 print(f"{str * 3}\n{str[0:9]}")
+print("\n")
+
+# Concat
+
+str1 = "Holberton"
+str2 = "School"
+print(f"Welcome to {str1 + ' ' + str2}!")
