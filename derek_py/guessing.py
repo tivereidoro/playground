0bf2f9ba7bdae86ec33e3748a1f4085a4a7857b9
tivereidoro <tivereidoro@yahoo.com>
2024-02-01 14:56:12 +0100

    Guess what

---
+++ derek_py/guessing.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+# Creating a guessing game
+print()
+print("Welcome again pal, let's play.\n")
+secret_number = 7
+
+while True:
+    try:
+        guess = int(input("Guess a number tween 1 and 10 : "))
+
+        if guess == secret_number:
+            print("\nHurray!! You guessed the right number.\n")
+            break
+        else:
+            print("\nNot quite right.. Try again!\n")
+            continue
+
+    except ValueError:
+        print("\nOops, that's not a number. Type a number tween 1 and 10 : \n")
+    except:
+        print("\nAn unknown error occured.\n")
