36687428b13d6b468c322f7982ee0dd328016d44
tivereidoro <tivereidoro@yahoo.com>
2024-05-02 18:26:55 +0100

    Calculate leap year

---
+++ jennys_pyCourse/leap_year.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Check whether a given year is leap year or not.
+
+year = int(input("Enter a year: "))
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"The year {year} is a leap year.")
+        else:
+            print(f"The year {year} is not a leap year.")
+    else:
+        print(f"The year {year} is a leap year.")
+else:
+    print(f"The year {year} is not a leap year.")
