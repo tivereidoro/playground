97949eb538f4b6175cf07c2ad913ec2580e81c4a
tivereidoro <tivereidoro@yahoo.com>
2024-06-05 21:21:11 +0100

    Progress: Optimum performance

--- jennys_pyCourse/quiz_game/main.py
+++ jennys_pyCourse/quiz_game/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-print("###### WELCOME TO QUIZ GAME ######\n")
+print("\n###### WELCOME TO QUIZ GAME ######\n")
 print("#######################################\n")
 
 # Questions
@@ -8,24 +8,32 @@ print("#######################################\n")
 # answers = ["Paris", "Berlin", "Madrid"]
 
 questions_db = [
-    {question: "What is the capital of France?",
-     options: "A. Paris\nB. New Jersey\nC. Berlin",
-     answer: "A"},
-     {question: "What is the capital of Germany?", options: "A. Paris\nB. Berlin\nC. New Jersey", answer: "B"},
+    {"question": "What is the capital of France?",
+     "options": "A. Paris\nB. New Jersey\nC. Berlin",
+     "answer": "A"},
+     
+    {"question": "What is the capital of Germany?",
+    "options": "A. Paris\nB. Berlin\nC. New Jersey",
+    "answer": "B"},
 ]
 
 # Score
 score = 0
 
 # Loop through questions
-for question in questions:
-    print(question)
-    user_answer = input("Your answer: ")
+for question in questions_db:
+    print(question["question"])
+    print(question["options"])
+    user_answer = input("Your answer: ").upper()
     
-    if user_answer in answers:
+    if user_answer == question["answer"]:
         score += 1
-        print("Correct!")
+        print(f"Correct! Your score is: {score}/2\n")
     else:
         print("You got that wrong!")
+        print(f"The correct answer is: {question['answer']}\n")
+
+# Final score
+print(f"Your final score is: {score}/2\n")
 
 
