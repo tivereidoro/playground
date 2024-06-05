c5e82bd56803aeb5ba77a115960126cebb93db55
tivereidoro <tivereidoro@yahoo.com>
2024-06-05 20:40:54 +0100

    Loop thru questions

--- jennys_pyCourse/quiz_game/main.py
+++ jennys_pyCourse/quiz_game/main.py
@@ -10,5 +10,14 @@ answers = ["Paris", "Berlin", "Madrid"]
 # Score
 score = 0
 
+# Loop through questions
+for question in questions:
+    print(question)
+    user_answer = input("Your answer: ")
+    if user_answer in answers:
+        print("Correct!")
+        score += 1
+    else:
+        print("You got that wrong!")
 
 
