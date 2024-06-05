4280ff32cea6e2017710782ff325a44a74f02217
tivereidoro <tivereidoro@yahoo.com>
2024-06-05 20:39:01 +0100

    Initiate few questions and answers

--- jennys_pyCourse/quiz_game/main.py
+++ jennys_pyCourse/quiz_game/main.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
 print("###### WELCOME TO YOUR QUIZ GAME ######")
+print("#######################################")
+
+# Questions
+questions = ["What is the capital of France?", "What is the capital of Germany?", "What is the capital of Spain?"]
+answers = ["Paris", "Berlin", "Madrid"]
+
+# Score
+score = 0
+
 
 
