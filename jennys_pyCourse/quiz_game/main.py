938ef971d2bf48cb86d94a288626493d9251f291
tivereidoro <tivereidoro@yahoo.com>
2024-06-05 21:06:19 +0100

    Using a list of  dicts for questions

--- jennys_pyCourse/quiz_game/main.py
+++ jennys_pyCourse/quiz_game/main.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
-print("###### WELCOME TO YOUR QUIZ GAME ######")
-print("#######################################")
+print("###### WELCOME TO QUIZ GAME ######\n")
+print("#######################################\n")
 
 # Questions
-questions = ["What is the capital of France?", "What is the capital of Germany?", "What is the capital of Spain?"]
-answers = ["Paris", "Berlin", "Madrid"]
+# questions = ["What is the capital of France?", "What is the capital of Germany?", "What is the capital of Spain?"]
+# answers = ["Paris", "Berlin", "Madrid"]
+
+questions_db = [
+    {question: "What is the capital of France?",
+     options: "A. Paris\nB. New Jersey\nC. Berlin",
+     answer: "A"},
+     {question: "What is the capital of Germany?", options: "A. Paris\nB. Berlin\nC. New Jersey", answer: "B"},
+]
 
 # Score
 score = 0
