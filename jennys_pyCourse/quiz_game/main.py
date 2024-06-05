2fbd1a7bebb73ca2c28ad7d895fc3a6401ae253a
tivereidoro <tivereidoro@yahoo.com>
2024-06-05 23:20:44 +0100

    Success: Quiz fully functional

--- jennys_pyCourse/quiz_game/main.py
+++ jennys_pyCourse/quiz_game/main.py
@@ -1,39 +1,36 @@
 #!/usr/bin/python3
+from questions import questions_db
 
 print("\n###### WELCOME TO QUIZ GAME ######\n")
 print("#######################################\n")
 
-# Questions
-# questions = ["What is the capital of France?", "What is the capital of Germany?", "What is the capital of Spain?"]
-# answers = ["Paris", "Berlin", "Madrid"]
-
-questions_db = [
-    {"question": "What is the capital of France?",
-     "options": "A. Paris\nB. New Jersey\nC. Berlin",
-     "answer": "A"},
-     
-    {"question": "What is the capital of Germany?",
-    "options": "A. Paris\nB. Berlin\nC. New Jersey",
-    "answer": "B"},
-]
-
 # Score
-score = 0
+number_of_questions = len(questions_db)
+right_answers = 0
 
 # Loop through questions
 for question in questions_db:
     print(question["question"])
     print(question["options"])
+    print()
     user_answer = input("Your answer: ").upper()
+
+    q_count = questions_db.index(question) + 1
     
     if user_answer == question["answer"]:
-        score += 1
-        print(f"Correct! Your score is: {score}/2\n")
+        right_answers += 1
+        print("Correct! Way to go!\n")
     else:
         print("You got that wrong!")
         print(f"The correct answer is: {question['answer']}\n")
 
+    print(f"Your current score is: {right_answers}/{q_count}\n")
+
+
+print("#########################################\n")
+print("###### That's the end of the quiz! ######\n")
 # Final score
-print(f"Your final score is: {score}/2\n")
+print(f"You got {right_answers} question(s) right out of {number_of_questions}\n")
+print(f"Your final score is: {right_answers / number_of_questions * 100}%\n")
 
 
---
+++ jennys_pyCourse/quiz_game/questions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+questions_db = [
+    {"question": "What is the capital of France?",
+     "options": "A. Paris\nB. New Jersey\nC. Berlin",
+     "answer": "A"},
+
+    {"question": "What is the capital of Germany?",
+    "options": "A. Paris\nB. Berlin\nC. New Jersey",
+    "answer": "B"},
+]
