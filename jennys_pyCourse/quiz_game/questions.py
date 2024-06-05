a3bc06ccf9b3ce456df5dfe805b938b91bafb449
tivereidoro <tivereidoro@yahoo.com>
2024-06-05 23:24:56 +0100

    Modularise the questions bank

--- jennys_pyCourse/quiz_game/questions.py
+++ jennys_pyCourse/quiz_game/questions.py
@@ -3,9 +3,18 @@
 questions_db = [
     {"question": "What is the capital of France?",
      "options": "A. Paris\nB. New Jersey\nC. Berlin",
-     "answer": "A"},
-
+     "answer": "A"
+    },
     {"question": "What is the capital of Germany?",
     "options": "A. Paris\nB. Berlin\nC. New Jersey",
-    "answer": "B"},
+    "answer": "B"
+    },
+    {"question": "What is the capital of USA?",
+    "options": "A. Washington DC\nB. New Jersey\nC. Berlin",
+    "answer": "A"
+    },
+    {"question": "What is the capital of China?",
+    "options": "A. Paris\nB. Beijing\nC. Berlin",
+    "answer": "B"
+    },
 ]
