03a397dd0af51b96c39e8ab555ba8c3a8065a52f
tivereidoro <tivereidoro@yahoo.com>
2024-06-06 21:09:44 +0100

    Update word list to import random module

--- jennys_pyCourse/word_list.py
+++ jennys_pyCourse/word_list.py
@@ -1,3 +1,5 @@
+import random
+
 words = ['apple', 'jet', 'nigeria', 'africa', 'intelligence', 'native',
          'Jesus', 'bible', 'church', 'blue', 'yellow', 'growth', 'market',
          'technology', 'java', 'python', 'github', 'brand']
