d2041548c90c14de34d33609b6a4be3b27cb41e0
tivereidoro <tivereidoro@yahoo.com>
2024-06-06 20:59:01 +0100

    Updates

---
+++ jennys_pyCourse/abstraction/abc.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class Bike:
+    def start():
+        print('Start with kick')
+    
+
+class Scooter:
+    def start():
+        print('Start scooter with self')
+
+class Car:
+    def start():
+        print('Start with key')
