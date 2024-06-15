6423870ded59cceb000d997cb83ac9cc32f871a8
tivereidoro <tivereidoro@yahoo.com>
2024-06-15 15:32:44 +0100

    Still remixing: practices on db and oop

---
+++ db/input_action.sql
@@ -0,0 +1,13 @@
+INSERT INTO student VALUE(
+    'Testco',
+    'Vue',
+    'newtestco@gmail.com',
+    '+234-802-3498-490',
+    'Delta',
+    'Nigeria',
+    '1990-02-21',
+    'F',
+    '4.21',
+    NOW(),
+    NULL
+);
--- db/lists_all.sql
+++ db/lists_all.sql
@@ -1,1 +1,18 @@
-show databases;
+-- Lists all dbs
+
+CREATE TABLE student(
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(40) NOT NULL,
+    email VARCHAR(60) NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    state VARCHAR(20) NOT NULL,
+    country VARCHAR(15) NOT NULL,
+    d_o_b DATE NOT NULL,
+    sex ENUM('M', 'F') NOT NULL,
+    CGPA FLOAT NULL,
+    start_date TIMESTAMP,
+    student_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY);
+
+SHOW TABLES;
+
+DESCRIBE student;
