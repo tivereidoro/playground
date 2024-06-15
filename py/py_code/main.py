6ab1d33aecda04ef7dcf0a4edcd50b4df285d490
tivereidoro <tivereidoro@yahoo.com>
2024-06-15 15:40:25 +0100

    Set up magic and init methods

--- py/py_code/main.py
+++ py/py_code/main.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 
-class Square:
-    pass
+class Customer:
+    """ My demo class for practicing
+    OOP with python.
+    """
 
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
-print(Square.__dict__)
+    def __init__(self, name, membership_type) -> None:
+        """ Initialisation method.
+        """
+        self.name = name
+        self.membership_type = membership_type
+
+    def __str__(self) -> str:
+        """ Stringify member instance.
+        """
+        return f"Customer: {self.name}\nSubscription: {self.membership_type} member."
+    
+    def 
+
