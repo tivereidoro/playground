1316d96fd85daabf8e2d5742460d74261fe09be5
tivereidoro <tivereidoro@yahoo.com>
2024-06-29 08:06:05 +0100

    Refactoring code base to optimise the App component

--- rxt/rxt-play/src/main.jsx
+++ rxt/rxt-play/src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
